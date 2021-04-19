c93a3759c7247cd8edf03b3c48b3cb04f2bbe4ab
dedsec-bug <69110150+dedsec-bug@users.noreply.github.com>
2021-04-19 15:27:47 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,19 +3,17 @@
 mkdir ~/tools
 cd ~/tools
 
+#sometimes these files are required
+echo -e "\e[1;33m[+] Installing requirements\e[0m"
+sudo apt-get install libpcap-dev
+
 echo -e "\e[1;33m[+] Installing naabu\e[0m" 
-git clone https://github.com/projectdiscovery/naabu.git
-cd naabu/v2/cmd/
-go build
-sudo cp naabu /usr/local/bin
+GO111MODULE=on go get -v github.com/projectdiscovery/naabu/v2/cmd/naabu
 naabu -version
 cd ~/tools
 
 echo -e "\e[1;33m[+] Installing HTTPX\e[0m"
-git clone https://github.com/projectdiscovery/httpx.git
-cd httpx/cmd
-go build
-sudo cp httpx /usr/local/bin
+GO111MODULE=on go get -v github.com/projectdiscovery/httpx/cmd/httpx
 httpx -version
 cd ~/tools
 
@@ -27,4 +25,5 @@ cd ~/tools
 echo -e "\e[1;33m[+] Installing Notify\e[0"
 GO111MODULE=on go get -v github.com/projectdiscovery/notify/cmd/notify
 GO111MODULE=on go get -v github.com/projectdiscovery/notify/cmd/intercept
+
 echo -e "\e[1;33m[+] Add configuration to this file : $HOME/.config/notify/notify.conf\e[0"

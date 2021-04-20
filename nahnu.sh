b910a1010e0954f8bc9ac3f43fc50a174927a418
dedsec-bug <69110150+dedsec-bug@users.noreply.github.com>
2021-04-20 21:23:31 +0530

    Update nahnu.sh

--- nahnu.sh
+++ nahnu.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
+if [ -d ~/recon/$1/$(date +"%d-%m-%Y") ]
+then
+        echo -e "\e[1;33m[+] Direcotry already exists\e[0m"
+else
+        mkdir -p ~/recon/$1/$(date +"%d-%m-%Y")
+        echo -e "\e[1;33m[+] Direcotry Created\e[0m"
+fi
+
 echo -e "\e[1;33m[+] Running naabu\e[0m"
-cat cidr.txt | naabu -p- -verify -o ports.txt
+cat ~/recon/$1/$(date +"%d-%m-%Y")/cidr.txt | naabu -p- -verify -o ~/recon/$1/$(date +"%d-%m-%Y")/ports.txt
 
 echo -e "\e[1;33m[+] Running HTTPX\e[0m"
-cat ports.txt | httpx -title -status-code -web-server -vhost
+cat ports.txt | httpx -title -status-code -web-server -vhost >> ~/recon/$1/$(date +"%d-%m-%Y")/httpx.txt
 
 echo -e "\e[1;33m[+] Running nuclei\e[0m"
-cat cidr.txt | naabu -silent -p - | httpx -silent | nuclei -t ~/tools/ nuclei-templates/ | notify
+cat cidr.txt | naabu -silent -p - | httpx -silent | nuclei -t ~/tools/ nuclei-templates/ >> ~/recon/$1/$(date +"%d-%m-%Y")/nuclei.txt
